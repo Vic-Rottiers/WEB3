a8c124b38f5c7de99855771e9671e476ee720157
Vic Rottiers <vic.rottiers@student.hogent.be>
2020-09-28 09:36:43 +0200

    Blackjackgame toegevoegd

---
+++ 04BlackJackGame/04BlackJackGame/Models/Domain/Card.cs
@@ -0,0 +1,23 @@
+﻿using BlackJackGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04BlackJackGame.Models.Domain
+{
+    class Card
+    {
+        #region properties
+        public FaceValue FaceValue { get; set; }
+        public Suit Suit { get; set; }
+
+        #endregion
+
+        #region constructors
+        public Card(Suit suit, FaceValue faceValue)
+        {
+            throw new NotImplementedException;
+        }
+        #endregion
+    }
+}
