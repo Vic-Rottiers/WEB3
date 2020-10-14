ad4a613aa71605ae43d342355f6613461e2f77b5
Vic Rottiers <vic.rottiers@student.hogent.be>
2020-10-14 09:37:50 +0200

    project helloMVC toegevoegd en mee geprutst

---
+++ 05HelloMVC/05HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string About()
+        {
+            return "bruh about bruh";
+        }
+    }
+}
